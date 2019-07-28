d54d6759790b71358aa842517e52b688085170ab
pcjangir <pcjangir2@gmail.com>
2019-07-28 18:36:12 +0530

    Initial Commit

---
+++ DownloadTaskSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DownloadTaskSwift
+//
+//  Created by Poonam on 28/07/19.
+//  Copyright © 2019 Poonam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
